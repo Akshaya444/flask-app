60e0a2bcbd23f6f406a3718f6b6392dd6c87eddb
Akshaya444 <akshayaputti7@gmail.com>
2025-05-12 16:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,4 +281,9 @@ def create_interface():
 
 if __name__ == "__main__":
     interface = create_interface()
-    interface.launch(server_name="0.0.0.0", server_port=PORT)
+    interface.launch(
+        server_name="0.0.0.0",
+        server_port=PORT,
+        share=False,
+        inbrowser=False
+    )
